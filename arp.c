f00aef030281dfe1328f80b6bffd494983b9e133
BMAN <554565808@qq.com>
2019-05-24 18:33:11 +0800

    test

--- arp.c
+++ arp.c
@@ -32,6 +32,8 @@ void arp_send_request(iface_info_t *iface, u32 dst_ip)
 	arp_req_pkt->arp_hln = ETH_ALEN;
 	arp_req_pkt->arp_pln = sizeof(dst_ip);   // todo: 这里是protocol address length？
 	memcpy(packet, arp_req_pkt, sizeof(struct ether_arp));
+	printf("= - =\n");
+	printf("size of packet is %d", (int)strlen(packet));
 	printf("%s", packet);
 }
 
