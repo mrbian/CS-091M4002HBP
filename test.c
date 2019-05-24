2733f2714e2b10e4dc3980ec4c1f67ef0ead3a29
BMAN <554565808@qq.com>
2019-05-24 09:19:19 +0800

    test

--- test.c
+++ test.c
@@ -34,14 +34,14 @@ void print_arp_cache_list() {
     printf("print all cached packet \n");
     struct arp_req *req = NULL;
     struct cached_pkt *pkt = NULL;
-//    list_for_each_entry(req, &arpcache.req_list, list) {
-//        printf("???\n");
+    list_for_each_entry(req, &arpcache.req_list, list) {
+        printf("???\n");
 //        pkt = NULL;
 //        list_for_each_entry(pkt, &req->cached_packets, list) {
 //            printf("Packet is %s \n", pkt->packet);
 //            printf("what the fuck");
 //        }
-//    }
+    }
 }
 
 // arpcache_test
