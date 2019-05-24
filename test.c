344c23f65486e68b7af117e8f8c886753910ab5e
BMAN <554565808@qq.com>
2019-05-24 11:18:47 +0800

    test

--- test.c
+++ test.c
@@ -44,18 +44,19 @@ void arpcache_test(){
     int result = arpcache_lookup(ip4, mac);  // should be not found
     if(result == 0) {
         printf("未找到IP为%s的MAC地址映射，应将此包加入待决包列表\n", ip_str);
-        arpcache_append_packet(iface, ip4, packet, (int)strlen(packet));  // 再加两个包
+        arpcache_append_packet(iface, ip4, packet, (int)strlen(packet));
     }
 
+    // 再加两个包
     ip4 = 0xA002;  // 10.0.0.2
     packet = "test2";
     arpcache_append_packet(iface, ip4, packet, (int)strlen(packet));
-
     ip4 = 0xA001;  // 10.0.0.1
     packet = "test3";
     arpcache_append_packet(iface, ip4, packet, (int)strlen(packet));
     print_arp_cache_list();
 
+    // 新建一个cache，测试arpcache_insert函数
     arpcache_insert(ip4, mac);
     print_arp_cache_list();
 }
