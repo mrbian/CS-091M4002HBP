8db7eeab1e67a07ab014b15f5b7a9f9f82ac8d3b
BMAN <554565808@qq.com>
2019-05-24 11:30:21 +0800

    test

--- arpcache.c
+++ arpcache.c
@@ -157,7 +157,8 @@ void arpcache_insert(u32 ip4, u8 mac[ETH_ALEN])
 			pkt = NULL;
 			list_for_each_entry(pkt, &req->cached_packets, list) {   // 填充好MAC地址然后依次发送出去
 				for(i = 0; i < ETH_ALEN; i += 1) { 					 // todo: 这样填充header是否正确？
-//					sprintf(macstr, "%d", mac[i]);
+					sprintf(macstr, "%d", mac[i]);
+					printf("%s", macstr);
 //					strcat(macstr, pkt->packet);
 				}
 				pkt->len = (int)strlen(pkt->packet);
