767cca2a919426660dd6b63a6c204f6c721208a2
BMAN <554565808@qq.com>
2019-06-03 18:08:47 +0800

    提交测试

--- ip.c
+++ ip.c
@@ -38,7 +38,9 @@ rt_entry_t *longest_prefix_match(u32 dst)
 	rt_entry_t * ele = NULL;
 	list_for_each_entry(ele, &rtable, list) {
 		if((ele->dest & ele->mask) == (dst & ele->mask)) {				// 如果按位与有相同的
+			printf("found \n");
 			if(ele->mask >= max_mask) {							// 如果本子网掩码是最大的
+				printf("set \n");
 				max_mask = ele->mask;
 				result = ele;
 			}
