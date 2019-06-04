10263fe6de01993ac0bfaaa3a13e8241b1ad0988
BMAN <554565808@qq.com>
2019-06-04 21:10:20 +0800

    代码基本完成

--- arpcache.c
+++ arpcache.c
@@ -62,7 +62,7 @@ int arpcache_lookup(u32 ip4, u8 mac[ETH_ALEN])
     int i = 0;
     int flag = 0;  // 默认未找到: 0
     for (i = 0; i < MAX_ARP_SIZE; i += 1) {
-		printf("entry ip: %x, valid: %d \n", arpcache.entries[i].ip4, arpcache.entries[i].valid);
+        //		printf("entry ip: %x, valid: %d \n", arpcache.entries[i].ip4, arpcache.entries[i].valid);
         if(arpcache.entries[i].ip4 == ip4 && arpcache.entries[i].valid == 1) {  // 如果找到且有效
             flag = 1;                           // flag置为找到: 1
             memcpy(mac, arpcache.entries[i].mac, sizeof(u8) * ETH_ALEN);  // MAC地址赋值
