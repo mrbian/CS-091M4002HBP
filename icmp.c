ea833728153816c546e31ad76f049cfca045956f
BMAN <554565808@qq.com>
2019-06-04 10:09:52 +0800

    提交测试

--- icmp.c
+++ icmp.c
@@ -10,7 +10,7 @@
 #include <base.h>
 
 // send icmp packet
-// 根据in_pkt（发送方的包）新建一个icmp包利用ip_send_packet发出去
+// 根据in_pkt（发送方的包）新建一个icmp包发出去
 // const是告诉coder，这里不能free和修改
 // 为什么会有第一个参数？不是应该根据type、code、dst_ip直接发吗？
 // 如果第一个参数是纯数据的话，应该有dst_ip参数，但是没有，所以第一个参数是别的包(⊙﹏⊙)b
@@ -32,7 +32,7 @@ void icmp_send_packet(const char *in_pkt, int len, u8 type, u8 code)
 		size_t packet_length = ETHER_HDR_SIZE + sizeof(struct iphdr) + sizeof(struct icmphdr);
 		char * packet = (char *)malloc(packet_length);
 		struct iphdr * packet_iphdr = packet_to_ip_hdr(packet);
-		ip_init_hdr(packet_iphdr, rt->iface->ip, pkt_ip_hdr->daddr, sizeof(struct iphdr) + sizeof(struct icmphdr), 1);   // protocal IPPROTO_ICMP : 1
+		ip_init_hdr(packet_iphdr, rt->iface->ip, pkt_ip_hdr->saddr, sizeof(struct iphdr) + sizeof(struct icmphdr), 1);   // protocal IPPROTO_ICMP : 1
 
 		// icmp header
 		struct icmphdr * packet_icmphdr = (struct icmphdr *)(packet + ETHER_HDR_SIZE + packet_iphdr->ihl * 4);
@@ -40,6 +40,7 @@ void icmp_send_packet(const char *in_pkt, int len, u8 type, u8 code)
 		packet_icmphdr->type = type;
 		packet_icmphdr->checksum = icmp_checksum(packet_icmphdr, sizeof(struct icmphdr));		// 要在最后面设置checksum
 
-		// 发送，这里不能用iface_send_packet_by_arp！
+		// 发送
+        iface_send_packet_by_arp(rt->iface, pkt_ip_hdr->saddr, packet, (int)packet_length);
 	}
 }
