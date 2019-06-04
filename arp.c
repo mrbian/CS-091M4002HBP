1860a26af436c397394c5af986caec8c53c35615
BMAN <554565808@qq.com>
2019-06-04 11:53:11 +0800

    提交测试

--- arp.c
+++ arp.c
@@ -102,7 +102,7 @@ void handle_arp_packet(iface_info_t *iface, char *packet, int len)
 
     if(ea->arp_op == ARPOP_REQUEST) {
         if(ea->arp_spa == iface->ip) {              // 如果是本机发出的arp请求包，则直接忽略
-            printf("是本机发出的arp请求包，丢弃");
+            printf("是本机发出的arp请求包，丢弃\n");
             return;
         }
         // 根据arp请求源添加arpcache表项
@@ -115,8 +115,8 @@ void handle_arp_packet(iface_info_t *iface, char *packet, int len)
             // todo
         }
     } else if(ea->arp_op == ARPOP_REPLY){
-        if(ea->arp_tpa == iface->ip) {              // 如果是本机发出的arp请求包，则直接忽略
-            printf("是本机发出的arp回复包，丢弃");
+        if(ea->arp_spa == iface->ip) {              // 如果是本机发出的arp请求包，则直接忽略
+            printf("是本机发出的arp回复包，丢弃\n");
             return;
         }
         arpcache_insert(ea->arp_tpa, ea->arp_tha);              // 将查询结果插入ARP表
