735fe62030287882570d2e68dc1a9390971d6cb2
BMAN <554565808@qq.com>
2019-06-04 23:47:29 +0800

    实验第一部分基本完成

--- icmp.c
+++ icmp.c
@@ -20,37 +20,70 @@ void icmp_send_packet(const char *in_pkt, int len, u8 type, u8 code)
 //	fprintf(stderr, "TODO: malloc and send icmp packet.\n");
 	// 获取到ip目的地址
 	struct iphdr * pkt_ip_hdr = packet_to_ip_hdr(in_pkt);
-	struct icmphdr * pkt_icmp_hdr;
+	struct icmphdr * pkt_icmp_hdr = (struct icmphdr *)(in_pkt + ETHER_HDR_SIZE + pkt_ip_hdr->ihl * 4);
 
 	rt_entry_t * rt = longest_prefix_match(ntohl(pkt_ip_hdr->saddr));
 	if(rt) {
-		// malloc an icmp packet
-		// ip header
-		size_t packet_length = ETHER_HDR_SIZE + sizeof(struct iphdr) + sizeof(struct icmphdr) + sizeof(struct iphdr);  // ICMP协议要将原包的IP的header添加到icmp header的后面，所以有两份ip header的空间
-		char * packet = (char *)malloc(packet_length);
-		struct iphdr * packet_iphdr = packet_to_ip_hdr(packet);
-		ip_init_hdr(packet_iphdr, rt->iface->ip, ntohl(pkt_ip_hdr->saddr), sizeof(struct iphdr) + sizeof(struct icmphdr) + sizeof(struct iphdr), 1);   // protocal IPPROTO_ICMP : 1
-
-		// icmp header
-		struct icmphdr * packet_icmphdr = (struct icmphdr *)(packet + ETHER_HDR_SIZE + packet_iphdr->ihl * 4);
-		packet_icmphdr->code = code;
-		packet_icmphdr->type = type;
-		memcpy(packet_icmphdr + sizeof(struct icmphdr), pkt_ip_hdr, sizeof(struct iphdr));								// 将原包头填充到icmp的数据域内
+		size_t packet_length;
+		char * packet;
+		struct iphdr * packet_iphdr;
+		struct icmphdr * packet_icmphdr;
 
 		// 判断类型对数据域进行分类处理
 		switch(type) {
-			case 0:
-				pkt_icmp_hdr = (struct icmphdr *)(in_pkt + ETHER_HDR_SIZE + pkt_ip_hdr->ihl * 4);
+			case 0:						// icmp回复
+				// malloc an icmp packet
+				// ip header
+				packet_length = (size_t)len;  // ICMP协议要将原包的IP的header添加到icmp header的后面，所以有两份ip header的空间
+				packet = (char *)malloc(packet_length);
+				packet_iphdr = packet_to_ip_hdr(packet);
+				ip_init_hdr(packet_iphdr, rt->iface->ip, ntohl(pkt_ip_hdr->saddr), (u16)(packet_length - sizeof(struct ether_header)), 1);   // protocal IPPROTO_ICMP : 1
+
+				// icmp header
+				packet_icmphdr = (struct icmphdr *)(packet + ETHER_HDR_SIZE + packet_iphdr->ihl * 4);
+				packet_icmphdr->code = code;
+				packet_icmphdr->type = type;
+
 				packet_icmphdr->icmp_identifier = pkt_icmp_hdr->icmp_identifier;
 				packet_icmphdr->icmp_sequence = pkt_icmp_hdr->icmp_sequence;
+				memcpy(packet_icmphdr + sizeof(struct icmphdr), pkt_icmp_hdr + sizeof(struct icmphdr), len - ETHER_HDR_SIZE - pkt_ip_hdr->ihl * 4 - sizeof(struct icmphdr));	// 需要request包的所有数据
 				break;
-			case 3:
+			case 3:						// 目的不可达
+				// malloc an icmp packet
+				// ip header
+				packet_length = (size_t)(ETHER_HDR_SIZE + sizeof(struct iphdr) + sizeof(struct icmphdr) + sizeof(struct iphdr));
+				packet = (char *)malloc(packet_length);
+				packet_iphdr = packet_to_ip_hdr(packet);
+				ip_init_hdr(packet_iphdr, rt->iface->ip, ntohl(pkt_ip_hdr->saddr), (u16)(packet_length - sizeof(struct ether_header)), 1);   // protocal IPPROTO_ICMP : 1
+
+				// icmp header
+				packet_icmphdr = (struct icmphdr *)(packet + ETHER_HDR_SIZE + packet_iphdr->ihl * 4);
+				packet_icmphdr->code = code;
+				packet_icmphdr->type = type;
+
+				memcpy(packet_icmphdr + sizeof(struct icmphdr), pkt_ip_hdr, sizeof(struct iphdr));								// 需要原包的ip header和64bit的数据
 				break;
-			case 11:
+			case 8:						// icmp请求
+				// todo
+				break;
+			case 11:				// 超时
+				// malloc an icmp packet
+				// ip header
+				packet_length = (size_t)(ETHER_HDR_SIZE + sizeof(struct iphdr) + sizeof(struct icmphdr) + sizeof(struct iphdr));
+				packet = (char *)malloc(packet_length);
+				packet_iphdr = packet_to_ip_hdr(packet);
+				ip_init_hdr(packet_iphdr, rt->iface->ip, ntohl(pkt_ip_hdr->saddr), (u16)(packet_length - sizeof(struct ether_header)), 1);   // protocal IPPROTO_ICMP : 1
+
+				// icmp header
+				packet_icmphdr = (struct icmphdr *)(packet + ETHER_HDR_SIZE + packet_iphdr->ihl * 4);
+				packet_icmphdr->code = code;
+				packet_icmphdr->type = type;
+
+				memcpy(packet_icmphdr + sizeof(struct icmphdr), pkt_ip_hdr, sizeof(struct iphdr));								// 需要原包的ip header和64bit的数据
 				break;
 			default:
 				printf("unknown icmp type! \n");
-				break;
+				return;
 		}
 
 		packet_icmphdr->checksum = icmp_checksum(packet_icmphdr, sizeof(struct icmphdr));		// 要在最后面设置checksum
