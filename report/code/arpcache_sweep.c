1804cea18dca71f4a18f8241f1de1c170ac6f4ed
BMAN <554565808@qq.com>
2019-06-05 17:33:26 +0800

    开始写报告

--- report/code/arpcache_sweep.c
+++ report/code/arpcache_sweep.c
@@ -13,24 +13,26 @@ void *arpcache_sweep(void *arg)
         }
 
         // 遍历待决包，如果等待时间超过1s，重发
-        // 如果重发次数超过5次，针对此包发送icmp包，并且删除掉此包
-        struct arp_req *req = NULL;
-        struct cached_pkt *pkt = NULL;
-        list_for_each_entry(req, &arpcache.req_list, list) {
+        // 如果重发次数超过5次，针对此包发送icmp包
+        struct arp_req *req = NULL, *req_q;
+        struct cached_pkt *pkt = NULL, *pkt_q;
+        list_for_each_entry_safe(req, req_q, &(arpcache.req_list), list) {
             if(req->retries >= 5) {
                 pkt = NULL;
-                list_for_each_entry(pkt, &req->cached_packets, list) {
+                list_for_each_entry_safe(pkt, pkt_q, &(req->cached_packets), list) {
                     printf("arp request failed, send icmp packet\n");
                     icmp_send_packet(pkt->packet, pkt->len, 3, 1);
+                    list_delete_entry(&(pkt->list));
                 }
-                list_delete_entry(&req->list);
+                list_delete_entry(&(req->list));
             } else {
                 time(&now);
-                if((long)now - (long)req->sent > 1) {
+                if((long)now - (long)req->sent >= 1) {
                     printf("arp request retry one more time\n");
                     req->retries += 1;
-                    req->sent = now;
                     arp_send_request(req->iface, req->ip4);
+                } else {
+                    printf("now is %ld, req->sent is %ld \n", (long)now, (long)req->sent);
                 }
             }
         }
