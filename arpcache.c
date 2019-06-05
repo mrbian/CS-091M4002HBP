a366fe85898b59337d1b274f85c068aeb59a6c7b
BMAN <554565808@qq.com>
2019-06-05 17:23:35 +0800

    开始写报告

--- arpcache.c
+++ arpcache.c
@@ -168,10 +168,10 @@ void arpcache_insert(u32 ip4, u8 mac[ETH_ALEN])
                 memcpy(eh->ether_dhost, mac, ETH_ALEN);
                 iface_send_packet(req->iface, pkt->packet, pkt->len);
                 list_delete_entry(&(pkt->list));                             // 从链表上删除
-                free(pkt);
+//                free(pkt);
 			}
             list_delete_entry(&(req->list));                                  // 删除此类待决包
-            free(req);
+//            free(req);
 		}
 	}
 }
@@ -210,10 +210,10 @@ void *arpcache_sweep(void *arg)
                     printf("arp request failed, send icmp packet\n");
                     icmp_send_packet(pkt->packet, pkt->len, 3, 1);  // type = 3, code = 1, 告知arp查询失败
                     list_delete_entry(&(pkt->list));
-                    free(pkt);
+//                    free(pkt);
                 }
                 list_delete_entry(&(req->list));          // 删除该项
-                free(req);
+//                free(req);
             } else {
                 time(&now);
                 if((long)now - (long)req->sent > 1) {  // 如果超时1s，重发arp请求，且重发次数+1，重发时间重置
